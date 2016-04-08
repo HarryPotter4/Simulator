55776953873c54dc088ef4d4b7d3f0c35e325f6c
Marius Becherer <marius.becherer@gmx.net>
2016-04-09 00:21:40 +0200

    Button event is successful and reachable

--- Simulation/Simulation/ViewModels/MainViewModel.cs
+++ Simulation/Simulation/ViewModels/MainViewModel.cs
@@ -11,9 +11,25 @@ namespace Simulation.ViewModels
     class MainViewModel : Screen
     {
 
-        public void button()
+        public void btn_play()
         {
             Debug.WriteLine("Button läuft!");
+        }
+        public void btn_next()
+        {
+            Debug.WriteLine("Button läuft!");
+        }
+        public void btn_pause()
+        {
+            Debug.WriteLine("Button läuft!");
+        }
+        public void btn_back()
+        {
+            Debug.WriteLine("Button läuft!");
+        }
+        public void menuItem_Open()
+        {
+            Debug.WriteLine("Open läuft!");
         }
     }
 }
