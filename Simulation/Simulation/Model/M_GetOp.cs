5dc4f6e56d1b0c8a943b004bd556c8efd229433c
preissdavid.de@gmail.com <preissdavid.de@gmail.com>
2016-04-12 11:31:24 +0200

    test

---
+++ Simulation/Simulation/Model/M_GetOp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.Model
+{
+    class M_GetOp
+    {
+        int argument = 0;
+        int op = 0;
+
+
+    }
+}
