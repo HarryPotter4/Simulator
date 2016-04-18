5aa6cc7b6952aabb02836169deb1c586105f1fbb
Marius Becherer <marius.becherer@gmx.net>
2016-04-18 22:43:32 +0200

    Create Operands and schedule them

--- Simulation/Simulation/Model/M_ProgramExecution.cs
+++ Simulation/Simulation/Model/M_ProgramExecution.cs
@@ -12,6 +12,8 @@ namespace Simulation.Model
         private List<M_FileListItem> _listItems;
         private M_Operators command;
         private RamViewModel ramViewModel;
+        private int programCounter;
+        private int executionCode;
         
 
 
@@ -27,21 +29,18 @@ namespace Simulation.Model
             }
             
         }
-
         private void startProgram()
         {
             foreach(M_FileListItem listItem in _listItems)
             {
-                nextMachineCycle(listItem.ProgramCounter, listItem.OpCode);
+                programCounter = Convert.ToInt32(listItem.ProgramCounter);
+                executionCode = Convert.ToInt32(listItem.OpCode);
+                nextMachineCycle(listItem.OpCode);
             }
         }
 
-        private void nextMachineCycle(string programCounter, string opCode)
-        {
-            findOperation(opCode);
-        }
 
-        private void findOperation(string opCode)
+        private void nextMachineCycle(string opCode)
         {
             int code = Convert.ToInt32(opCode);
 
@@ -65,7 +64,6 @@ namespace Simulation.Model
             {
                 select_Bit_Operations(code);
             }
-
         }
 
         private void select_BO_Command(int opCode)
@@ -83,7 +81,7 @@ namespace Simulation.Model
             switch (operationCode)
             {
                 case 0:
-                    specialCases(fileRegister);
+                    specialCases(destinationsBit, fileRegister);
                     break;
                 case 1:
                     if (destinationsBit == 0)
@@ -93,45 +91,209 @@ namespace Simulation.Model
                     else
                         Console.WriteLine("Case1 of CLRW/F");
                     break;
-
                 case 2:
                     command.subwf(destinationsBit, fileRegister);
                     break;
-
                 case 3:
                     command.decf(destinationsBit, fileRegister);
                     break;
-
                 case 4:
                     command.iOrWf(destinationsBit, fileRegister);
                     break;
-
-                case 2:
+                case 5:
+                    command.andWf(destinationsBit, fileRegister);
+                    break;
+                case 6:
+                    command.xorWF(destinationsBit, fileRegister);
+                    break;
+                case 7:
+                    command.addWf(destinationsBit, fileRegister);
+                    break;
+                case 8:
+                    command.movF(destinationsBit, fileRegister);
+                    break;
+                case 9:
+                    command.compF(destinationsBit, fileRegister);
+                    break;                
+                case 10:
+                    command.incF(destinationsBit, fileRegister);
+                    break;
+                case 11:
+                    command.decfsz(destinationsBit, fileRegister);
+                    break;
+                case 12:
+                    command.rrf(destinationsBit, fileRegister);
+                    break;
+                case 13:
+                    command.rlf(destinationsBit, fileRegister);
+                    break;
+                case 14:
+                    command.swapf(destinationsBit, fileRegister);
+                    break;
+                case 15:
+                    command.incfsz(destinationsBit, fileRegister);
                     break;
 
                 default:
+                    Console.WriteLine("Error in switch of byte-orientated file register; value of operationCode: " + operationCode);
                     break;
             }
   
 
-            throw new NotImplementedException();
+           
+        }
+        private void specialCases(int destinationsBit, int fileRegister)
+        {
+            if(destinationsBit == 1)
+            {
+                command.movWF(destinationsBit = 1, fileRegister);
+            }
+            else if((fileRegister & convertBinToInt("0000_0000_0001_1111")) == 0 )
+            {
+                command.nop(destinationsBit = 0, fileRegister);
+            }
+            else if((fileRegister & convertBinToInt("0000_0000_0111_1111")) == 100)
+            {
+                command.clrwdt();
+            }
+            else if ((fileRegister & convertBinToInt("0000_0000_0111_1111")) == 9)
+            {
+                command.retIEF();
+            }
+            else if ((fileRegister & convertBinToInt("0000_0000_0111_1111")) == 8)
+            {
+                command.reTurn();
+            }
+            else if ((fileRegister & convertBinToInt("0000_0000_0111_1111")) == 99)
+            {
+                command.sleep();
+            }
         }
 
-        private void specialCases(int fileRegister)
+        private void select_LIT_Operations(int opCode)
         {
-            throw new NotImplementedException();
-        }
+            int code = Convert.ToInt32(opCode);
 
-        private void select_LIT_Operations(int code)
+            int operationCodeMask = convertBinToInt("0011_0000_0000_0000");
+            int operationCode = (code & operationCodeMask) / Convert.ToInt32(Math.Pow(2, 12));
+
+            int constValueMask = convertBinToInt("0000_0111_1111_1111");
+            int constValue = (code & constValueMask);
+
+            int controlMask = convertBinToInt("0000_1000_0000_0000");
+            int controlCode = (code & controlMask) / Convert.ToInt32(Math.Pow(2, 11));
+
+            if (operationCode == 3)
+            {
+                select_Literals(opCode);
+            }
+            else if (operationCode == 2)
+            {
+                if((code & controlCode) == 1)
+                {
+                    command.goTo(constValue);
+                }
+                else if((code & controlCode) == 0)
+                {
+                    command.call(constValue);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error in select_LIT_Operations");
+            }    
+        }
+        private void select_Literals(int code)
         {
-            throw new NotImplementedException();
+            int constValueMask = convertBinToInt("0000_0000_1111_1111");
+            int codeMask = convertBinToInt("0000_1111_0000_0000");
+
+            int constValue = (code & constValueMask);
+            int operationCode = (code & codeMask) / Convert.ToInt32(Math.Pow(2, 8));
+            
+            switch (operationCode)
+            {
+                case 0:
+                    command.movLW(constValue);
+                    break;
+                case 1:
+                    command.movLW(constValue);
+                    break;
+                case 2:
+                    command.movLW(constValue);
+                    break;
+                case 3:
+                    command.movLW(constValue);
+                    break;
+                case 4:
+                    command.retLW(constValue);                    
+                    break;
+                case 5:
+                    command.retLW(constValue);
+                    break;
+                case 6:
+                    command.retLW(constValue);
+                    break;
+                case 7:
+                    command.retLW(constValue);
+                    break;
+                case 8:
+                    command.iorLW(constValue);
+                    break;
+                case 9:
+                    command.andLW(constValue);
+                    break;
+                case 10:
+                    command.xorLW(constValue);
+                    break;                
+                case 12:
+                    command.subLW(constValue);
+                    break;
+                case 13:
+                    command.subwf(constValue);
+                    break;
+                case 14:
+                    command.addLW(constValue);
+                    break;
+                case 15:
+                    command.addLW(constValue);
+                    break;
+                default:
+                    Console.WriteLine("Error in switch of byte-orientated file register; value of operationCode: " + operationCode);
+                    break;
+            }
         }
 
         private void select_Bit_Operations(int code)
         {
-            throw new NotImplementedException();
-        }
+            int codeMask = convertBinToInt("0000_1100_0000_0000");
+            int bitMask = convertBinToInt("0000_0011_1000_0000");
+            int fileMask = convertBinToInt("0000_0000_0111_1111");
+            
+            int operationCode = (code & codeMask) / Convert.ToInt32(Math.Pow(2, 10));
+            int selectedBit = (code & bitMask) / Convert.ToInt32(Math.Pow(2,7));
+            int fileRegister = (code & fileMask);
 
+            switch (operationCode)
+            {
+                case 0:
+                    command.bcf(fileRegister, selectedBit);
+                    break;
+                case 1:
+                    command.bsf(fileRegister, selectedBit);
+                    break;
+                case 2:
+                    command.btfsc(fileRegister, selectedBit);
+                    break;
+                case 3:
+                    command.btfss(fileRegister, selectedBit);
+                    break;         
+                              
+                default:
+                    Console.WriteLine("Error in switch of bit-orientated file register; value of operationCode: " + operationCode);
+                    break;
+            }
+        }    
         private int convertBinToInt(string bin)
         {
             bin = bin.Replace("_", "");
@@ -143,7 +305,6 @@ namespace Simulation.Model
         {
 
         }
-
         private void getArgument()
         {
 
