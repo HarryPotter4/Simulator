25867343b1dbb6bfc741778bd56eb0cb6b745c29
Marius Becherer <marius.becherer@gmx.net>
2016-04-25 15:10:26 +0200

    Add io pin view

---
+++ Simulation/Simulation/ViewModels/IOPinsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Simulation.ViewModels
+{
+    class IOPinsViewModel : Screen
+    {
+        public void portA_pin7()
+        {
+            MessageBox.Show("Hallow ");
+        }
+
+        private bool _Checkbox_Pin7;
+
+        public bool Checkbox_Pin7
+        {
+            get
+            {
+                return _Checkbox_Pin7;
+            }
+
+            set
+            {
+                _Checkbox_Pin7 = value;
+                NotifyOfPropertyChange(() => Checkbox_Pin7);
+            }
+        }
+    }
+}
