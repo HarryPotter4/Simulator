54acc7caea90f6101c83c862ab5d8922ec0dcb9c
Marius Becherer <marius.becherer@gmx.net>
2016-04-25 16:02:48 +0200

    Try to bind ramModel to CHeckbox

--- Simulation/Simulation/ViewModels/IOPinsViewModel.cs
+++ Simulation/Simulation/ViewModels/IOPinsViewModel.cs
@@ -16,6 +16,12 @@ namespace Simulation.ViewModels
         }
 
         private bool _Checkbox_PortA_Pin7;
+        private RamViewModel ramViewModel;
+
+        public IOPinsViewModel(RamViewModel ramViewModel)
+        {
+            this.ramViewModel = ramViewModel;
+        }
 
         public bool Checkbox_PortA_Pin7
         {
@@ -27,7 +33,20 @@ namespace Simulation.ViewModels
             set
             {
                 _Checkbox_PortA_Pin7 = value;
+                ramViewModel.setBit(0, 5, 4, getIntValue(Checkbox_PortA_Pin7));
                 NotifyOfPropertyChange(() => Checkbox_PortA_Pin7);
+            }
+        }
+
+        private int getIntValue(bool boolValue )
+        {
+            if (boolValue)
+                return 1;
+            else if (!boolValue)
+                return 0;
+            else
+            {
+                throw new NotImplementedException();
             }
         }
     }
